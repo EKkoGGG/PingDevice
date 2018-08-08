c481a8f097862c2bb94afe5cf4062da421657e5e
YoZengWeijie <330564052@qq.com>
2018-08-08 09:58:03 +0800

    + 添加配置文档

--- PingDevice/PingDevice/Program.cs
+++ PingDevice/PingDevice/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MySql.Data.MySqlClient;
 using System.Net.NetworkInformation;
 using System.Data;
+using System.Configuration;
 
 namespace PingDevice
 {
@@ -12,7 +13,7 @@ namespace PingDevice
     {
         static void Main(string[] args)
         {
-            string conStr = "server =183.47.15.146; port=3306; user=root; password=joysw377136; database=db_jinruivms;";
+            string conStr = ConfigurationManager.ConnectionStrings["MySqlconnString"].ConnectionString;
             MySqlConnection connMySql = new MySqlConnection(conStr);
             string sql = "Select vpn_address,id From tb_device";
             MySqlDataAdapter sda = new MySqlDataAdapter(sql, connMySql);
@@ -25,30 +26,37 @@ namespace PingDevice
                 {
                     for (int i = 0; i < dt.Rows.Count; i++)
                     {
-                        Ping ping = new Ping();
-                        PingReply pingReply = ping.Send(dt.Rows[i]["vpn_address"].ToString());
-                        if (pingReply.Status == IPStatus.Success)
+                        if (dt.Rows[i]["vpn_address"].ToString() != "")
                         {
-                            //Console.WriteLine("当前在线，已PING通！");
-                            connMySql.Open();
-                            string comStr = "UPDATE tb_device SET `online` = 1 WHERE id ='" + dt.Rows[i]["id"].ToString() + "' ";
-                            MySqlCommand comm = new MySqlCommand(comStr, connMySql);
-                            int count = comm.ExecuteNonQuery();
-                            connMySql.Close();
+                            Ping ping = new Ping();
+                            PingReply pingReply = ping.Send(dt.Rows[i]["vpn_address"].ToString());
+                            if (pingReply.Status == IPStatus.Success)
+                            {
+                                //Console.WriteLine("当前在线，已PING通！");
+                                connMySql.Open();
+                                string comStr = "UPDATE tb_device SET `online` = 1 WHERE id ='" + dt.Rows[i]["id"].ToString() + "' ";
+                                MySqlCommand comm = new MySqlCommand(comStr, connMySql);
+                                int count = comm.ExecuteNonQuery();
+                                connMySql.Close();
+                            }
+                            else
+                            {
+                                //Console.WriteLine("不在线，PING不通！");
+                                connMySql.Open();
+                                string comStr = "UPDATE tb_device SET `online` = 0 WHERE id ='" + dt.Rows[i]["id"].ToString() + "' ";
+                                MySqlCommand comm = new MySqlCommand(comStr, connMySql);
+                                int count = comm.ExecuteNonQuery();
+                                connMySql.Close();
+                            }
                         }
                         else
                         {
-                            //Console.WriteLine("不在线，PING不通！");
-                            connMySql.Open();
-                            string comStr = "UPDATE tb_device SET `online` = 0 WHERE id ='" + dt.Rows[i]["id"].ToString() + "' ";
-                            MySqlCommand comm = new MySqlCommand(comStr, connMySql);
-                            int count = comm.ExecuteNonQuery();
-                            connMySql.Close();
+                            continue;
                         }
                     }
                 }
                 catch (Exception)
-                {             
+                {
                     Console.WriteLine("错误，请检查设备地址是否填写正确");
                 }
 
